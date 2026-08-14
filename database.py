d6efb0ca3fabe24b781f2063ee25dc73a9fe4fa0
Ajithsagarhs <example@gmail.com>
2026-08-14 21:53:56 +0530

    Complete Job Application Tracker

--- database.py
+++ database.py
@@ -1,81 +1,47 @@
 import sqlite3
 
+
 DATABASE = "jobs.db"
 
 
-def get_connection():
+def create_database():
+
     conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
+
+    cursor = conn.cursor()
 
 
-def create_table():
-    conn = get_connection()
-
-    conn.execute("""
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS jobs (
+
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+
             company TEXT NOT NULL,
+
             role TEXT NOT NULL,
-            application_date TEXT,
-            status TEXT,
+
+            application_date TEXT NOT NULL,
+
+            status TEXT NOT NULL,
+
             interview_date TEXT,
+
             job_url TEXT,
+
             notes TEXT
+
         )
     """)
 
-    conn.commit()
-    conn.close()
-
-
-def add_job(company, role, application_date, status,
-            interview_date, job_url, notes):
-
-    conn = get_connection()
-
-    conn.execute("""
-        INSERT INTO jobs
-        (company, role, application_date, status,
-         interview_date, job_url, notes)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    """, (
-        company,
-        role,
-        application_date,
-        status,
-        interview_date,
-        job_url,
-        notes
-    ))
 
     conn.commit()
-    conn.close()
-
-
-def get_all_jobs():
-    conn = get_connection()
-
-    jobs = conn.execute("""
-        SELECT * FROM jobs
-        ORDER BY id DESC
-    """).fetchall()
 
     conn.close()
 
-    return jobs
+
+    print("Database and jobs table created successfully!")
 
 
-def delete_job(job_id):
-    conn = get_connection()
+if __name__ == "__main__":
 
-    conn.execute(
-        "DELETE FROM jobs WHERE id = ?",
-        (job_id,)
-    )
-
-    conn.commit()
-    conn.close()
-
-
-create_table()
+    create_database()
